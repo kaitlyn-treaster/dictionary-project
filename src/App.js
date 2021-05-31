17b70c084a4a1b4e9dd3a8821f36a9dac80b1601
KTreaster04 <79948666+KTreaster04@users.noreply.github.com>
2021-05-31 12:58:35 -0400

    Started to make the search form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-
+import Dictionary from "./Dictionary";
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header"></header>
-      Hello
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer"><small>Coded by Kaitlyn Treaster</small></footer>
+      </div>
     </div>
   );
 }
