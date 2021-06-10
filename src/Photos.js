d8f5918f488f33fd0e63dadf37287d0586c117a4
KTreaster04 <79948666+KTreaster04@users.noreply.github.com>
2021-06-10 10:45:24 -0400

    Added Photo component

--- src/Photos.js
+++ src/Photos.js
@@ -1,5 +1,22 @@
 import React from "react";
 
 export default function Photos(props) {
-    return "Hello from Photos";
+    if (props.photos) {
+        return  (
+            <section className="Photos">
+                <div className="row">
+                    {props.photos.map(function(photo, index) {
+                        return (
+                            <div className="col-4" key={index}>
+                                <img src={photo.src.landscape} 
+                                className="img-fluid" />
+                            </div>
+                        );
+                    })}
+                </div>
+            </section>
+        );
+    } else{
+        return null;
+    }
 }
