8e03955ba941d2f31bea4973430335e7f507dc36
KTreaster04 <79948666+KTreaster04@users.noreply.github.com>
2021-06-11 11:15:34 -0400

    Added photos and made changes to styling

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -7,17 +7,26 @@ import "./Dictionary.css";
 export default function Dictionary() {
     let [keyword, setKeyword] = useState("");
     let [results, setResults] = useState(null);
+    let [photos, setPhotos] = useState(null);
 
-    function handleResponse(response) {
+    function handleDictionResponse(response) {
         setResults(response.data[0]);
+    }
 
+    function handlePexelsResponse(response) {
+        setPhotos(response.data.photos);
     }
 
     function search(event) {
         event.preventDefault();
 
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-        axios.get(apiUrl).then(handleResponse);
+        axios.get(apiUrl).then(handleDictionResponse);
+
+        let pexelsApiKey = "563492ad6f917000010000019682d717c38c40cda7ffd038a2b9233b";
+        let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
+        let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+        axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
     }
 
     function handleKeywordChange(event) {
@@ -34,7 +43,8 @@ export default function Dictionary() {
             </form>
             <br />
             <Results results={results} />
-            <Photos />
+            <br />
+            <Photos photos={photos} />
             
             
         </div>
