2cd87e88169de0016d53a9f40c161a83d2394e5b
KTreaster04 <79948666+KTreaster04@users.noreply.github.com>
2021-06-10 21:25:03 -0400

    Update Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -13,8 +13,8 @@ export default function Dictionary() {
 
     }
 
-    function search() {
-        
+    function search(event) {
+        event.preventDefault();
 
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
         axios.get(apiUrl).then(handleResponse);
@@ -35,6 +35,7 @@ export default function Dictionary() {
             <br />
             <Results results={results} />
             <Photos />
+            
             
         </div>
     );
